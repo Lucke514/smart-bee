9c75dbe3b7dca6b19b8a3a078c14bc7b5a00509d
Lucke514 <lucas.gonzalez.espinosa@outlook.com>
2025-07-16 20:41:36 -0400

    feat: implement signIn function for user authentication service

---
+++ services/auth.service.ts
@@ -0,0 +1,10 @@
+/**
+ * @author Lucas Gonzalez
+ * @description Servicio de autenticación para manejar el inicio de sesión de usuarios.
+ * Este servicio se encarga de autenticar a los usuarios mediante su correo electrónico y contraseña.
+ * @param email - Email del usuario
+ * @param password - Contraseña del usuario
+ */
+export async function signIn(email : string, password : string) {
+
+}
