8641154a816ae4b4e9780b6ea77064338904be4b
Lucke514 <lucas.gonzalez.espinosa@outlook.com>
2025-07-16 20:39:57 -0400

    feat: replace main content with Header component and remove unused components

---
+++ components/header.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+
+export default function Header() {
+    return (
+        <View className="w-full p-4 bg-yellow-500">
+            <Text className="text-white text-lg font-bold">Smart Bee</Text>
+        </View>
+    )
+}
