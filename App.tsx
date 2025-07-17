6b5d32d4ee3a0c2a205829977f2d8af021055970
Lucke514 <lucas.gonzalez.espinosa@outlook.com>
2025-07-16 20:17:09 -0400

    Initial commit

--- App.tsx
+++ App.tsx
@@ -1,13 +1,10 @@
-import { ScreenContent } from 'components/ScreenContent';
-import { StatusBar } from 'expo-status-bar';
-
 import './global.css';
+import { Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <>
-      <ScreenContent title="Home" path="App.tsx"></ScreenContent>
-      <StatusBar style="auto" />
-    </>
+    <View className="flex-1 items-center justify-center bg-white">
+      <Text className="text-2sxl font-bold text-blue-500">Welcome to Nativewind!</Text>
+    </View>
   );
 }
